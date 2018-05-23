5152437ac8b4f0d842583be9ef8552bf23d7e73a
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2018-05-23 16:04:50 +0200

    simplify the example code

--- src/main/java/org/knime/knip/Resize.java
+++ src/main/java/org/knime/knip/Resize.java
@@ -3,11 +3,8 @@ package org.knime.knip;
 
 import io.scif.SCIFIO;
 
-import java.util.concurrent.Future;
-
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
-import net.imagej.ops.OpService;
 import net.imagej.ops.Ops;
 import net.imglib2.FinalInterval;
 import net.imglib2.Interval;
@@ -16,68 +13,15 @@ import net.imglib2.img.ImgView;
 import net.imglib2.interpolation.randomaccess.NearestNeighborInterpolatorFactory;
 import net.imglib2.realtransform.RealViews;
 import net.imglib2.realtransform.Scale;
-import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
 
-import org.scijava.ItemIO;
-import org.scijava.command.Command;
-import org.scijava.command.CommandModule;
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
-import org.scijava.ui.UIService;
-import org.scijava.ui.UserInterface;
-
-/**
- * This example illustrates how to create an ImageJ {@link Command} plugin.
- * <p>
- * The code here is a simple Gaussian blur using ImageJ Ops.
- * </p>
- * <p>
- * You should replace the parameter fields with your own inputs and outputs, and
- * replace the {@link run} method implementation with your own logic.
- * </p>
- */
-@Plugin(type = Command.class, menuPath = "Plugins>Resize")
-public class Resize<T extends RealType<T>> implements Command {
-	//
-	// Feel free to add more parameters here...
-	//
-
-	@Parameter
-	private Dataset currentData;
-
-	@Parameter
-	private UIService uiService;
-
-	@Parameter
-	private OpService opService;
-
-	@Parameter(type = ItemIO.OUTPUT)
-	private Img<T> out;
-
-	@Override
-	public void run() {
-		final Img<T> inImg = (Img<T>) currentData.getImgPlus();
-
-		final double[] scaleFactors = { 0.9090909090909091, 2.0, 10.0,
-			6.666666666666667 };
-		final Interval resultingInterval = new FinalInterval(20, 20, 20, 20);
-
-		out = ImgView.wrap(Views.interval(Views.raster(RealViews.affineReal(Views
-			.interpolate(Views.extendBorder(inImg),
-				new NearestNeighborInterpolatorFactory<T>()), new Scale(scaleFactors))),
-			resultingInterval), inImg.factory());
-	}
+public class Resize {
 
 	/**
-	 * This main function serves for development purposes. It allows you to run
-	 * the plugin immediately out of your integrated development environment
-	 * (IDE).
-	 *
-	 * @param args whatever, it's ignored
-	 * @throws Exception
+	 * Demonstrates Imglib2 regression
 	 */
 	public static void main(final String... args) throws Exception {
+
 		// create the ImageJ application context with all available services
 		final ImageJ ij = new ImageJ();
 
@@ -85,14 +29,19 @@ public class Resize<T extends RealType<T>> implements Command {
 		final SCIFIO scifio = ij.scifio();
 		// load the dataset
 		final Dataset dataset = scifio.datasetIO().open("res/original.ome.tif");
+		final Img inImg = dataset.getImgPlus().getImg();
 
-		// show the image
-		ij.ui().show(dataset);
+		// the resizing
+		final double[] scaleFactors = { 0.9090909090909091, 2.0, 10.0,
+			6.666666666666667 };
+		final Interval resultingInterval = new FinalInterval(20, 20, 20, 20);
 
-		// invoke the plugin
-		final Future<CommandModule> commandRun = ij.command().run(Resize.class,
-			true);
-		final Img resized = (Img) commandRun.get().getOutput("out");
+		final Img resized = ImgView.wrap(Views.interval(Views.raster(RealViews
+			.affineReal(Views.interpolate(Views.extendBorder(inImg),
+				new NearestNeighborInterpolatorFactory()), new Scale(scaleFactors))),
+			resultingInterval), inImg.factory());
+
+		// testing the result
 
 		final Img newResizerResult = scifio.datasetIO().open(
 			"res/resizedNew.ome.tif").getImgPlus().getImg();
@@ -120,8 +69,6 @@ public class Resize<T extends RealType<T>> implements Command {
 			System.out.println("REGRESSION: difference to old resizer result: " +
 				sumOld);
 		}
-
-		System.exit(0);
 
 	}
 
